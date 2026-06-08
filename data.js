a970485d0ee674c647b427faf285e9ce9ab970b1
nishelbajra <nrb7239@mavs.uta.edu>
2026-06-08 04:56:20 -0500

    Update data.js

---
+++ data.js
@@ -0,0 +1,92 @@
+const characters = [
+  {
+    id: 1,
+    name: "Iron Man",
+    realName: "Tony Stark",
+    team: "Avengers",
+    ability: "Powered armor suit with advanced weapons",
+    firstAppearance: "Iron Man (2008)",
+    description: "A genius billionaire who built a powered suit of armor to save the world.",
+    image: "https://upload.wikimedia.org/wikipedia/en/4/47/Iron_Man_%28circa_2018%29.png",
+    threatLevel: "High"
+  },
+  {
+    id: 2,
+    name: "Captain America",
+    realName: "Steve Rogers",
+    team: "Avengers",
+    ability: "Super soldier strength, vibranium shield",
+    firstAppearance: "Captain America: The First Avenger (2011)",
+    description: "A WWII soldier transformed into the world's first super soldier.",
+    image: "https://upload.wikimedia.org/wikipedia/en/3/35/Captain_America_uniform.jpg",
+    threatLevel: "High"
+  },
+  {
+    id: 3,
+    name: "Thor",
+    realName: "Thor Odinson",
+    team: "Avengers",
+    ability: "God of Thunder, wields Mjolnir",
+    firstAppearance: "Thor (2011)",
+    description: "The Asgardian god of thunder and one of the most powerful Avengers.",
+    image: "https://upload.wikimedia.org/wikipedia/en/8/8a/Thor_Odinson.jpg",
+    threatLevel: "Cosmic"
+  },
+  {
+    id: 4,
+    name: "Black Widow",
+    realName: "Natasha Romanoff",
+    team: "Avengers",
+    ability: "Master spy, martial arts, Widow's Bite",
+    firstAppearance: "Iron Man 2 (2010)",
+    description: "A highly trained spy and assassin turned S.H.I.E.L.D. agent.",
+    image: "https://upload.wikimedia.org/wikipedia/en/9/99/Black_Widow_%28Natasha_Romanova%29.jpg",
+    threatLevel: "Medium"
+  },
+  {
+    id: 5,
+    name: "Thanos",
+    realName: "Thanos",
+    team: "Villain",
+    ability: "Infinity Gauntlet, superhuman strength",
+    firstAppearance: "Avengers: Infinity War (2018)",
+    description: "A warlord obsessed with wiping out half of all life in the universe.",
+    image: "https://upload.wikimedia.org/wikipedia/en/f/f6/Thanos_%28comics_character%29.png",
+    threatLevel: "Cosmic"
+  },
+  {
+    id: 6,
+    name: "Loki",
+    realName: "Loki Laufeyson",
+    team: "Villain",
+    ability: "Sorcery, shapeshifting, illusion casting",
+    firstAppearance: "Thor (2011)",
+    description: "The Asgardian god of mischief and Thor's adoptive brother.",
+    image: "https://upload.wikimedia.org/wikipedia/en/2/20/Loki_%28Marvel_Comics%29.jpg",
+    threatLevel: "High"
+  },
+  {
+    id: 7,
+    name: "Spider-Man",
+    realName: "Peter Parker",
+    team: "Avengers",
+    ability: "Wall-crawling, web-slinging, spider-sense",
+    firstAppearance: "Captain America: Civil War (2016)",
+    description: "A teenage genius bitten by a radioactive spider who protects New York City.",
+    image: "https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png",
+    threatLevel: "Medium"
+  },
+  {
+    id: 8,
+    name: "Black Panther",
+    realName: "T'Challa",
+    team: "Avengers",
+    ability: "Vibranium suit, enhanced strength and agility",
+    firstAppearance: "Captain America: Civil War (2016)",
+    description: "King of Wakanda and protector of his nation's vibranium resources.",
+    image: "https://upload.wikimedia.org/wikipedia/en/1/1b/Black_Panther_%28Marvel_Comics%29.png",
+    threatLevel: "High"
+  }
+];
+
+module.exports = characters;
